4ee2222a28abd8f94cfcea51a99fb083b2478eda
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-26 13:26:11 +0530

    Create 60.java

---
+++ 60.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=0;
+        for(int i=1;i<=n;i++){
+            a=a+i;
+        }
+        System.out.print(a);
+    }
+}
+
+		
+	
